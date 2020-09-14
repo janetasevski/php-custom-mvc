9efdee48e1ae6a53524a5b1918f3fe8de11caa4d
Jane Tasevski <janetasevski@MacBook-Pro.local>
2020-09-14 18:18:32 +0200

    imitial commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+//Database params
+define('DB_HOST', 'localhost');
+define('DB_USER', '_YOUR_USER_');
+define('DB_PASS', '_YOUR_PASS_');
+define('DB_NAME', '_YOUR_DB_NAME_');
+
+//App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL Root
+define('URLROOT', '_YOUT_URL_');
+
+//Site name
+define('SITENAME', '_YOUT_SITENAME_');
